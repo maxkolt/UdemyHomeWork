b00f863488b9063e012c3820965d7b7c8d4c014d
maxkolt <12345kolt@gmail.com>
2021-01-11 15:17:39 +0300

    -_- день 1-ый

--- js/script.js
+++ js/script.js
@@ -2,6 +2,14 @@
 const pi = Number(Math.PI.toFixed(2));
 console.log(pi)
 //
-const arr = Math.min(15, 11, 16, 12, 51, 12, 13, 51);
-console.log(arr);
+const arrMin = Math.min(15, 11, 16, 12, 51, 12, 13, 51);
+const arrMax = Math.max(15, 11, 16, 12, 51, 12, 13, 51);
+console.log(arrMin);
+console.log(arrMax);
 //
+const arrNum = Number(Math.random().toFixed(2));
+console.log(arrNum);
+//
+const num = 15;
+const a = Math.round(Math.random() * num);
+console.log(a)
