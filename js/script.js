b38cfd9a03de321a60fcff5f03f0478fd806cb79
maxkolt <12345kolt@gmail.com>
2021-01-11 19:04:15 +0300

    -_- 1ый день

--- js/script.js
+++ js/script.js
@@ -21,3 +21,31 @@ const numStr = parseFloat('100$');
 console.log(numStr);
 
 //2ая домашка по строкам
+let string = "some test string";
+const foo = string[0];
+const foot = string[string.length -1];
+console.log(foo);
+console.log(foot);
+// с этим кодом нужно разобраться еще раз
+const firstLastUpper = `${string[0].toUpperCase()}${string.slice(1,-1)}${string[string.length - 1].toUpperCase()}`;
+console.log(firstLastUpper);
+//
+const str = string.indexOf('string');
+console.log(str);
+//
+const index = string.indexOf(' ');
+const indexLen = string.indexOf(' ', index + 1);
+console.log(indexLen);
+//
+const substr = string.substr(5, 4);
+console.log(substr);
+//
+const ctr = string.slice(5, 10);
+console.log(ctr);
+//
+const fix = string.slice(0, -6);
+console.log(fix);
+//
+const i = 20, b = 21;
+const numSt = String(i) + String(b);
+console.log(numSt);
