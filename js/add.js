8a3c52785256888ef15e83b7d5d9cde90c09e2df
maxkolt <12345kolt@gmail.com>
2021-02-23 13:58:21 +0300

    async await

--- js/add.js
+++ js/add.js
@@ -1,10 +1,10 @@
 //Fetch
 fetch('http://jsonplaceholder.typicode.com/posts')
- .then(response => {
-     return response.json();
-})
- .then(post => console.log(post))
- .catch(err => console.log(err));
+    .then(response => {
+        return response.json();
+    })
+    .then(post => console.log(post))
+    .catch(err => console.log(err));
 
 //Promise
 function getPost(id) {
@@ -15,4 +15,17 @@ function getPost(id) {
             .catch(err => reject(err))
     });
 }
+
 getPost(1).then(post => console.log(post));
+
+//async await
+async function getPost2(id) {
+    const response = await fetch(
+        `http://jsonplaceholder.typicode.com/posts/${id}`,
+    ).then(res => res.json())
+    return response;
+}
+
+getPost2(1)
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
