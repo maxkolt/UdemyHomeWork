c45f8bd8236fd46c5c8baa7b3c25c6f3b4d986be
maxkolt <12345kolt@gmail.com>
2021-02-23 13:39:37 +0300

    Promise

--- js/add.js
+++ js/add.js
@@ -1,6 +1,18 @@
+//Fetch
 fetch('http://jsonplaceholder.typicode.com/posts')
  .then(response => {
      return response.json();
- })
+})
  .then(post => console.log(post))
  .catch(err => console.log(err));
+
+//Promise
+function getPost(id) {
+    return new Promise((resolve, reject) => {
+        fetch(`http://jsonplaceholder.typicode.com/posts/${id}`)
+            .then(response => response.json())
+            .then(post => resolve(post))
+            .catch(err => reject(err))
+    });
+}
+getPost(1).then(post => console.log(post));
